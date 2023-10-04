cdc0350b1ed88069739219904acbab03ebde6dd3
sclash <samuelpellikka@gmail.com>
2023-10-04 13:58:41 +0300

    leffakaruselli

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}", "./src/**/*.{html,js}",  "./node_modules/flowbite/**/*.js"],
   theme: {
     screens: {
       xs: "390px",
@@ -16,7 +16,7 @@ module.exports = {
         "bg-image": "url('../dist/img/bg.jpeg')",
       },
       fontFamily: {
-        'roboto-mono': ['Roboto Mono', 'monospace'],
+        body: ['Roboto Mono', 'monospace'],
       },
       colors: {
         "mm-orange": "rgba(255, 92, 0, 1)",
@@ -25,5 +25,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+],
 };
